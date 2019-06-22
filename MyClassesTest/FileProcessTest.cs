87b581ea258f5103bc9dc19ac53ea039d03982fc
Ida Naderian <aina21@users.noreply.github.com>
2019-06-22 16:45:33 +0200

    Created unit test project

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoeseExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameDoeseNotExist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00c82ed3-bc8a-4573-bddd-c781920f2ef8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
